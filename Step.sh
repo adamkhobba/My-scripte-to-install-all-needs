6bf904eedc64e3cabc41f63a29e654f09dca00b1
Mr. Adam KHOBBA <129398945+adamkhobba@users.noreply.github.com>
2025-11-06 20:28:03 -0500

    Comment out Chrome installation in Step.sh
    Comment out the installation commands for Google Chrome.

--- Step.sh
+++ Step.sh
@@ -50,8 +50,8 @@ git clone git@github.com:adamkhobba/obsidian.git
 mv obsidian Download
 
 # Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
